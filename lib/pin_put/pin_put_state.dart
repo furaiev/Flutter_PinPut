74eb84a60337d67bc43c0e105da0c16701044365
Oleksii Furaiev <furaiev@gmail.com>
2020-07-20 13:10:38 +0300

    Each field as flexible

--- lib/pin_put/pin_put_state.dart
+++ lib/pin_put/pin_put_state.dart
@@ -151,36 +151,39 @@ class PinPutState extends State<PinPut>
 
   Widget _getField(int index) {
     final String pin = _controller.value.text;
-    return AnimatedContainer(
-      width: widget.eachFieldWidth,
-      height: widget.eachFieldHeight,
-      alignment: Alignment.center,
-      duration: widget.animationDuration,
-      curve: widget.animationCurve,
-      padding: widget.eachFieldPadding,
-      margin: widget.eachFieldMargin,
-      constraints: widget.eachFieldConstraints,
-      decoration: _fieldDecoration(index),
-      child: AnimatedSwitcher(
-        switchInCurve: widget.animationCurve,
-        switchOutCurve: widget.animationCurve,
+    return Flexible(
+      child: AnimatedContainer(
+        width: widget.eachFieldWidth,
+        height: widget.eachFieldHeight,
+        alignment: Alignment.center,
         duration: widget.animationDuration,
-        transitionBuilder: (child, animation) {
-          return _getTransition(child, animation);
-        },
-        child: index < pin.length
-            ? Text(
-                widget.obscureText ?? pin[index],
-                key: ValueKey<String>(index < pin.length ? pin[index] : ''),
-                style: widget.textStyle,
-              )
-            : index == pin.length
-                ? _buildCursor()
-                : Text(
-                    widget.preFilledChar ?? '',
-                    key: ValueKey<String>(index < pin.length ? pin[index] : ''),
-                    style: widget.preFilledCharStyle ?? widget.textStyle,
-                  ),
+        curve: widget.animationCurve,
+        padding: widget.eachFieldPadding,
+        margin: widget.eachFieldMargin,
+        constraints: widget.eachFieldConstraints,
+        decoration: _fieldDecoration(index),
+        child: AnimatedSwitcher(
+          switchInCurve: widget.animationCurve,
+          switchOutCurve: widget.animationCurve,
+          duration: widget.animationDuration,
+          transitionBuilder: (child, animation) {
+            return _getTransition(child, animation);
+          },
+          child: index < pin.length
+              ? Text(
+                  widget.obscureText ?? pin[index],
+                  key: ValueKey<String>(index < pin.length ? pin[index] : ''),
+                  style: widget.textStyle,
+                )
+              : widget.withCursor && index == pin.length
+                  ? _buildCursor()
+                  : Text(
+                      widget.preFilledChar ?? '',
+                      key: ValueKey<String>(
+                          index < pin.length ? pin[index] : ''),
+                      style: widget.preFilledCharStyle ?? widget.textStyle,
+                    ),
+        ),
       ),
     );
   }
